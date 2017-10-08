69e89cb5f9aee627e71c878d8b27c8c1804eb39a
KennyTheWizard <kenngineering@gmail.com>
2017-10-07 19:24:47 -0500

    update dojo_survey with validation

--- dojo_survey/server.py
+++ dojo_survey/server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, flash, session
 
 app = Flask(__name__)
+app.secret_key = "YouAreGreat"
 
 @app.route('/')
 def index():
@@ -8,8 +9,38 @@ def index():
 
 @app.route('/results', methods=['POST'])
 def process():
-    return render_template('results.html',
-        name=request.form['name'], location=request.form['location'],
-        fav_lang=request.form['fav_lang'], comment=request.form['comment'])
+    name=request.form['name']
+    location=request.form['location']
+    fav_lang=request.form['fav_lang']
+    comment=request.form['comment']
+    input_valid = True
+
+    if len(name) == 0:
+        input_valid = False
+        flash("The name cannot be blank.")
+    if len(comment) == 0:
+        input_valid = False
+        flash("The commment field cannot be blank.")
+    if len(comment) > 120:
+        input_valid = False
+        flash("The comment cannot be longer than 120 characters")
+
+    if input_valid:
+        session['name'] = name
+        session['location'] = location
+        session['fav_lang'] = fav_lang
+        session['comment'] = comment
+        return redirect('/resultspage')
+    else:
+        return redirect('/')
+
+@app.route('/resultspage')
+def show_results():
+    return render_template('results.html')
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    session.clear()
+    return redirect('/')
 
 app.run(debug=True)
